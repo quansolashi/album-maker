5ead3e28fdc6ddb1befa8481bf97d30002e1f57a
quannv3 <quannv@solashi.com>
2023-04-19 11:16:24 +0700

    feat(backend): setup prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import appConfig from './config/app.config';
+import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [appConfig],
     }),
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
